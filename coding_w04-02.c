5059e72822c67b006e754695fb8a4d6d076e696e
Peerapat6812022611111 <s6812022611111@email.kmutnb.ac.th>
2025-07-16 19:13:37 +0700

    1111

--- coding_w04-02.c
+++ coding_w04-02.c
@@ -1,7 +1,7 @@
-#include <stdio.h>  
+#include <stdio.h>  //  ไม่มี # หน้า include
 
 int main() {
-    int a = 10;    
-    printf("a = %d", a);  
-    return 0;      
+    int a = 10;    // ไม่มี ; ปิดท้ายคำสั่ง
+    printf("a = %d", a);  // ไม่มี ; ปิดท้ายคำสั่ง
+    return 0;      // ไม่มี ; ปิดท้ายคำสั่ง
 }
